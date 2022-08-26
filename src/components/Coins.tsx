0e6e2f3dd553161dc2d294c24c7283787ab26bdb
fcbjimmy <jim.culers@gmail.com>
2022-08-27 01:15:36 +0800

    filter

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -1,15 +1,26 @@
 import { Result } from '../helper/data.types';
 import styles from './Coins.module.scss';
 import Coin from './Coin';
+import { useState } from 'react';
 
 interface Props {
   coins: Result[] | null;
 }
 
 const Coins = ({ coins }: Props) => {
-  console.log(coins);
+  const [findCoin, setFindCoin] = useState<string>('');
+
+  //add debounce
+  const searchCoin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setFindCoin(e.target.value);
+  };
+
   return (
     <>
+      <div className={styles.search}>
+        <input type='text' placeholder='search' onChange={searchCoin} />
+      </div>
       <section className={styles.container}>
         <table className={styles.tableContent}>
           <thead>
@@ -22,33 +33,41 @@ const Coins = ({ coins }: Props) => {
             </tr>
           </thead>
           <tbody>
-            {coins?.map((coin, index) => {
-              return (
-                <tr key={index}>
-                  <td data-label='Coin'>
-                    <span className={styles.image}>
-                      <img src={coin.image} alt={coin.name} />
-                      <div>
-                        <p>{coin.name}</p>
-                        <p>{coin.symbol}</p>
-                      </div>
-                    </span>
-                  </td>
-                  <td data-label='Price'>
-                    <span>${coin.current_price}</span>
-                  </td>
-                  <td data-label='24H'>
-                    <span>{coin.price_change_24h}</span>
-                  </td>
-                  <td data-label='Volume'>
-                    <span>{coin.price_change_percentage_24h}</span>
-                  </td>
-                  <td data-label='Market Cap'>
-                    <span>{coin.market_cap}</span>
-                  </td>
-                </tr>
-              );
-            })}
+            {coins
+              ?.filter((data) => {
+                if (findCoin === '') {
+                  return data;
+                } else if (data.name.toLowerCase().includes(findCoin)) {
+                  return data;
+                }
+              })
+              .map((coin, index) => {
+                return (
+                  <tr key={index}>
+                    <td data-label='Coin'>
+                      <span className={styles.image}>
+                        <img src={coin.image} alt={coin.name} />
+                        <div>
+                          <p>{coin.name}</p>
+                          <p>{coin.symbol}</p>
+                        </div>
+                      </span>
+                    </td>
+                    <td data-label='Price'>
+                      <span>${coin.current_price}</span>
+                    </td>
+                    <td data-label='24H'>
+                      <span>{coin.price_change_24h}</span>
+                    </td>
+                    <td data-label='Volume'>
+                      <span>{coin.price_change_percentage_24h}</span>
+                    </td>
+                    <td data-label='Market Cap'>
+                      <span>{coin.market_cap}</span>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </section>
